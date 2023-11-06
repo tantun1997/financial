cf5775ce561849dd2b03fcb2911e76a3c640f40c
tantun1997 <138105515+tantun1997@users.noreply.github.com>
2023-11-06 14:08:38 +0700

    Update maintenance-plan.blade.php

--- resources/views/livewire/maintenance-plan.blade.php
+++ resources/views/livewire/maintenance-plan.blade.php
@@ -357,7 +357,7 @@
                     title: `รายงานแผนฯบำรุงรักษา หน่วยบริการโรงพยาบาลสมเด็จพระพุทธเลิศหล้า`,
                     autoFilter: true,
                     exportOptions: {
-                        columns: [0, 1, 4, 5, 6, 7, 8, 19, 10, 11, 12, 13, 14, 15]
+                        columns: [0, 1, 4, 5, 6, 7, 8, 18, 10, 11, 12, 13, 14, 15]
                     },
                     className: 'btn btn-outline-success', // เพิ่มคลาส CSS เพื่อปรับแต่งสีปุ่ม
                     init: function(api, node, config) {
